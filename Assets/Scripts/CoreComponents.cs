0d3733308d949f32f3498bf8eb662a450e9a2961
tommydeboer <tommydeboer4@gmail.com>
2022-04-04 16:43:06 +0200

    Remove Grid

--- Assets/Scripts/CoreComponents.cs
+++ Assets/Scripts/CoreComponents.cs
@@ -4,21 +4,6 @@ namespace GridGame
 {
     public static class CoreComponents
     {
-        static Grid grid;
-
-        public static Grid Grid
-        {
-            get
-            {
-                if (grid == null)
-                {
-                    grid = GameObject.FindWithTag(Tags.GRID).GetComponent<Grid>();
-                }
-
-                return grid;
-            }
-        }
-
         static Game game;
 
         public static Game Game

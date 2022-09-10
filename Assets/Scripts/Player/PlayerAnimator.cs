5eafb4357802c81f69d8c15db5cc6de2ff0b194f
tommydeboer <tommydeboer4@gmail.com>
2022-09-10 12:07:22 +0200

    Add player tilt when moving

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -9,9 +9,6 @@ namespace GridGame.Player
     {
         [Header("Config")]
         [SerializeField]
-        Transform model;
-
-        [SerializeField]
         Transform pivot;
 
         [Header("Push Feedback")]
@@ -27,36 +24,99 @@ namespace GridGame.Player
         [SerializeField, Range(0f, .5f)]
         float maxPushDistance = .25f;
 
+        [Header("Tilt feedback")]
+        [SerializeField, Range(0f, .5f)]
+        float timeToMaxTilt = .1f;
+
+        [SerializeField, Range(0f, 45f)]
+        float maxTiltAngle = 10;
+
         Hero hero;
 
-        float moveTime;
-        float stopTime;
+        readonly ToAndFroTimer moveTime = new();
+        readonly ToAndFroTimer pushTime = new();
+        Vector3 pivotDefaultPosition;
         float pushAmount;
+        float tiltAmount;
         bool isPressingMove;
+
+        class ToAndFroTimer
+        {
+            public float ToTime { get; private set; }
+            public float FroTime { get; private set; }
+
+            public void Evaluate(bool condition)
+            {
+                if (condition)
+                {
+                    ToTime += Time.deltaTime;
+                    FroTime = 0f;
+                }
+                else
+                {
+                    FroTime += Time.deltaTime;
+                    ToTime = 0f;
+                }
+            }
+        }
 
         void Awake()
         {
             hero = GetComponent<Hero>();
+            pivotDefaultPosition = pivot.localPosition;
         }
 
         void Update()
         {
-            if (!isPressingMove)
+            moveTime.Evaluate(isPressingMove);
+            pushTime.Evaluate(hero.IsBlocked);
+            
+            if (hero.IsBlocked && !hero.OnClimbable)
             {
-                stopTime += Time.deltaTime;
-                moveTime = 0f;
-                pushAmount = stopTime / timeToReturnFromPush;
-
-                model.localPosition = Vector3.Lerp(model.localPosition, Vector3.zero, pushAmount);
+                Push();
             }
-            else if (hero.IsBlocked)
+            else
             {
-                moveTime += Time.deltaTime;
-                stopTime = 0f;
-                pushAmount = moveTime / timeToMaxPush;
-
-                model.localPosition = Vector3.forward * maxPushDistance * pushCurve.Evaluate(pushAmount);
+                ReturnFromPush();
             }
+
+            if (isPressingMove && !hero.OnClimbable)
+            {
+                Tilt();
+            }
+            else
+            {
+                ReturnFromTilt();
+            }
+        }
+
+        void Tilt()
+        {
+            tiltAmount = Mathf.Clamp(moveTime.ToTime / timeToMaxTilt, 0f, 1f);
+            float rotation = Mathf.SmoothStep(0, maxTiltAngle, tiltAmount);
+            pivot.localRotation = Quaternion.Euler(rotation, 0, 0);
+        }
+
+        void ReturnFromTilt()
+        {
+            if (tiltAmount == 0) return;
+            tiltAmount = 1 - Mathf.Clamp(moveTime.FroTime / timeToMaxTilt, 0f, 1f);
+            float rotation = Mathf.SmoothStep(0, maxTiltAngle, tiltAmount);
+            pivot.localRotation = Quaternion.Euler(rotation, 0, 0);
+        }
+
+        void Push()
+        {
+            pushAmount = pushTime.ToTime / timeToMaxPush;
+            pivot.localPosition = pivotDefaultPosition +
+                                  (Vector3.forward * maxPushDistance * pushCurve.Evaluate(pushAmount));
+        }
+
+        void ReturnFromPush()
+        {
+            pushAmount = pushTime.FroTime / timeToReturnFromPush;
+            var localPosition = pivot.localPosition;
+            pivot.localPosition = Vector3.Lerp(localPosition, pivotDefaultPosition, pushAmount);
         }
 
         [UsedImplicitly]
